6ac06a0ba8fc26579bf5be1d6f202aebc1d07a6c
Jimmy-Morzaria <morzaria.jimmy@gmail.com>
2014-08-06 23:11:27 +0530

    refined api

---
+++ src/main/java/org/cytoscape/cyGraph/algorithms/api/FloydWarshallResult.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package org.cytoscape.cyGraph.algorithms.api;
+
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNode;
+
+/**
+ * Provides methods to get shortest path between a given pair of nodes, negative
+ * cycle if one exists, etc. as computed by Floyd-Warshall algorithm.
+ * 
+ * @author Jimmy
+ * 
+ */
+public interface FloydWarshallResult {
+
+	/**
+	 * Checks whether the CyNetwork has negative cycles.
+	 * 
+	 * @return True if the CyNetwork has negative cycles and false otherwise.
+	 */
+	public boolean hasNegativeCycle();
+
+	/**
+	 * Checks if there is a path from the source CyNode to the target CyNode.
+	 * 
+	 * @param target
+	 *            Target CyNode.
+	 * @return True if there is path from the source CyNode to the target CyNode
+	 *         and false otherwise.
+	 */
+	public boolean hasPath(CyNode source, CyNode target);
+
+	/**
+	 * Returns the distance from the source CyNode to target CyNode.
+	 * 
+	 * @param source
+	 *            Source CyNode
+	 * @param target
+	 *            Target CyNode
+	 * @return Distance from the source CyNode to target CyNode.
+	 */
+	public double getDistance(CyNode source, CyNode target);
+
+	/**
+	 * Returns the shortest path from the source CyNode to the target CyNode as
+	 * a sequence of CyEdges.
+	 * 
+	 * @param source
+	 *            Source CyNode.
+	 * @param target
+	 *            Target CyNode.
+	 * @return Sequence of CyEdges from the source CyNode to the target CyNode
+	 *         constituting the shortest path.
+	 */
+	public Iterable<CyEdge> getPath(CyNode source, CyNode target);
+
+	/**
+	 * Returns a sequence of CyEdges from the source CyNode to the target CyNode
+	 * constituting the negative cycle.
+	 * 
+	 * @return Sequence of CyEdges from the source CyNode to the target CyNode
+	 *         constituting the negative cycle.
+	 */
+	public Iterable<CyEdge> getNegativeCycle();
+}
---
+++ src/main/java/org/cytoscape/cyGraph/algorithms/api/GraphAlgorithms.java
@@ -0,0 +1,189 @@
+/**
+ * 
+ */
+package org.cytoscape.cyGraph.algorithms.api;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+
+/**
+ * Graph Algorithms is the primary interface providing access to a number of
+ * graph algorithms. These include breadth first search, depth first search,
+ * Bellman-Ford, Dijkstra, Floyd-Warshall, Prim, Kruskal, Pagerank and HITS.
+ * 
+ * @author Jimmy
+ * 
+ */
+public interface GraphAlgorithms {
+
+	/**
+	 * Performs breadth first search on a CyNetwork starting from a given source
+	 * CyNode.
+	 * 
+	 * @param network
+	 *            CyNetwork containing the source node from which the user wants
+	 *            to perform breadth first search.
+	 * @param source
+	 *            Source CyNode.
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @param callback
+	 *            Callback function
+	 * @return
+	 */
+	public BfsResult breadthFirstSearch(CyNetwork network, CyNode source,
+			boolean directed, Callback callback);
+
+	/**
+	 * Performs depth first search on a CyNetwork starting from a given source
+	 * CyNode.
+	 * 
+	 * @param network
+	 *            CyNetwork containing the source node from which the user wants
+	 *            to perform depth first search.
+	 * @param source
+	 *            Source CyNode.
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @param callback
+	 *            Callback function
+	 * @return
+	 */
+	public DfsResult depthFirstSearch(CyNetwork network, CyNode source,
+			boolean directed, Callback callback);
+
+	/**
+	 * Computes all pairs shortest path on a CyNetwork using the Floyd Warshall
+	 * algorithm.
+	 * 
+	 * @param network
+	 *            CyNetwork
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @param function
+	 *            WeightFunction mapping each edge to a weight.
+	 * @return
+	 */
+	public FloydWarshallResult findAllPairShortestPath(CyNetwork network,
+			boolean directed, WeightFunction function);
+
+	/**
+	 * Finds the minimum spanning tree of a CyNetwork using Kruskal's algorithm.
+	 * 
+	 * @param network
+	 *            CyNetwork
+	 * @param function
+	 *            WeightFunction mapping each edge to a weight.
+	 * @return
+	 */
+	public MSTResult findKruskalTree(CyNetwork network, WeightFunction function);
+
+	/**
+	 * Computes the shortest paths from a source CyNode to all the other nodes
+	 * in a CyNetwork.
+	 * 
+	 * @param network
+	 *            CyNetwork
+	 * @param source
+	 *            Source CyNode.
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @param function
+	 *            WeightFunction mapping each edge to a weight.
+	 * @return
+	 */
+	public DijkstraResult findPath(CyNetwork network, CyNode source,
+			boolean directed, WeightFunction function);
+
+	/**
+	 * Finds the minimum spanning tree of a CyNetwork using Prim's algorithm.
+	 * 
+	 * @param network
+	 *            CyNetwork for which the user wants to find the minimum
+	 *            spanning tree.
+	 * @param function
+	 *            WeightFunction mapping each edge to a weight.
+	 * @return
+	 */
+	public MSTResult findPrimTree(CyNetwork network, WeightFunction function);
+
+	/**
+	 * Finds shortest paths from a source CyNode to all the other nodes in a
+	 * CyNetwork. Useful when the network contains negative cycles.
+	 * 
+	 * @param network
+	 *            CyNetwork containing the source node and the other nodes to
+	 *            which the user wants to find the shortest path.
+	 * @param source
+	 *            Source CyNode.
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @param function
+	 *            WeightFunction mapping each edge to a weight.
+	 * @return
+	 */
+	public BellmanFordResult findShortestPath(CyNetwork network, CyNode source,
+			boolean directed, WeightFunction function);
+
+	/**
+	 * Computes the hub and authority values of CyNodes in a CyNetwork.
+	 * 
+	 * @param network
+	 *            CyNetwork containing the nodes for which the user wants to
+	 *            compute HITS scores.
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @return An instance of HITSResults that provides methods to access the
+	 *         hub and authority values of a node.
+	 */
+	public HITSResults getHITSScores(CyNetwork network, boolean directed);
+
+	/**
+	 * Computes the pagerank of CyNodes in a CyNetwork.
+	 * 
+	 * @param network
+	 *            CyNetwork containing the nodes for which the user wants to
+	 *            compute pagerank scores.
+	 * @param function
+	 *            WeightFunction mapping each edge to a weight.
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @return An instance of PagerankResults that provides methods to access
+	 *         the pagerank score.
+	 */
+	public PagerankResults getPageranks(CyNetwork network,
+			WeightFunction function, boolean directed);
+
+	/**
+	 * Computes the pagerank of CyNodes in a CyNetwork with the probability and
+	 * epsilon value provided by the user.
+	 * 
+	 * @param network
+	 *            CyNetwork containing the nodes for which the user wants to
+	 *            compute pagerank scores.
+	 * @param function
+	 *            WeightFunction mapping each edge to a weight.
+	 * @param directed
+	 *            True if the network is to be treated as directed and false
+	 *            otherwise.
+	 * @param probability
+	 *            Probability value alpha to be used in the computation of
+	 *            pagerank scores. The default is 0.85.
+	 * @param epsilon
+	 *            Epsilon value to be used in pagerank computation. The default
+	 *            is 0.001.
+	 * @return An instance of PagerankResults that provides methods to access
+	 *         the pagerank score.
+	 */
+	public PagerankResults getPageranks(CyNetwork network,
+			WeightFunction function, boolean directed, double probability,
+			double epsilon);
+
+}
